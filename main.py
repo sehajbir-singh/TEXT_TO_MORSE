7639223b267e53c41f5a61aa278a2ce799f0a247
sehajbir-singh <sehajbirdhillon26@gmail.com>
2025-09-04 00:30:43 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '0': '-----', ' ': '/'
+}
+
+
+
+
+
+
+def text_to_morse(text):
+
+    text = text.upper();
+    morse_code = ' '.join(MORSE_CODE_DICT[char] for char in text if char in MORSE_CODE_DICT)
+    return morse_code
+
+
+stringin = input("Enter the string to convert it into morse code: ")
+output = text_to_morse(stringin)
+for ch in output:
+    if ch == '/':
+        output = output.replace("/", " ")
+print(output)
+
+
+
+
